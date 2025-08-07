0ce1b0d559b2a913945c44b4caefe756f1c14bfa
thuhang263 <nguyenthuhang26032003@gmail.com>
2025-08-07 08:41:35 +0700

    update_login

--- resources/views/admin/users/login.blade.php
+++ resources/views/admin/users/login.blade.php
@@ -7,12 +7,12 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="#"><b>Admin</b></a>
+        <a href="#"><b>Quản trị viên</b></a>
     </div>
     <!-- /.login-logo -->
     <div class="card">
         <div class="card-body login-card-body">
-            <p class="login-box-msg">Sign in to start your session</p>
+            <p class="login-box-msg">Đăng nhập</p>
             @include('admin.alert')
             <form action="/admin/users/login/store" method="post">
                 <div class="input-group mb-3">
@@ -41,25 +41,16 @@
                         </div>
                     </div>
                     <div class="col-4">
-                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                        <button type="submit" class="btn btn-primary btn-block">Đăng nhập</button>
                     </div>
                 </div>
                 @csrf
             </form>
-            <div class="social-auth-links text-center mb-3">
-                <p>- OR -</p>
-                <a href="#" class="btn btn-block btn-primary">
-                    <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-                </a>
-                <a href="#" class="btn btn-block btn-danger">
-                    <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-                </a>
-            </div>
             <p class="mb-1">
-                <a href="forgot-password.html">I forgot my password</a>
+                <a href="forgot-password.html">Quên mật khẩu</a>
             </p>
             <p class="mb-0">
-                <a href="register.html" class="text-center">Register a new membership</a>
+                <a href="register.html" class="text-center">Đăng ký</a>
             </p>
         </div>
     </div>
